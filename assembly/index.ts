87a938919b2293ec775d618bd788c573ae612f85
Yusuke Kawamoto <novogrammer@gmail.com>
2024-04-16 00:12:53 +0900

    Counter

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,23 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+
+class Counter{
+  value:i8;
+  constructor(){
+    this.value=0;
+  }
+  countUp():void{
+    this.value+=1;
+  }
+}
+const counter=new Counter();
+
+export function countUp():void{
+  counter.countUp();
+}
+export function getValue():i8{
+  return counter.value;
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import './style.css'
-import {add} from "./../build/release";
+import {memory,add,countUp,getValue} from "./../build/release";
 
 console.log("main.ts");
 
 console.log(`add(1,2): ${add(1,2)}`);
+
+console.log(`memory.buffer.byteLength: ${memory.buffer.byteLength}`);
+
+
+for(let i=0;i<256;i++){
+  countUp();
+  console.log(`getValue(): ${getValue()}`);
+}
 
